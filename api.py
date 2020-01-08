06caf0bfba074270e5b82242586b418f19a5c5ad
IanFennell <ianfennell1@gmail.com>
2020-01-08 04:06:53 +0000

    Initial Commit

---
+++ api.py
@@ -0,0 +1,108 @@
+import core
+import os
+import shutil
+from mido import MidiFile
+
+def RemoveScopeError(Exception):
+    pass
+
+instructions = {"single":core.remove_single_instrument,
+                "range":core.remove_instrument_range}
+
+def main():
+    # remember to:
+    # -clean up code
+    # -Set wait to play? Simple os.sleep
+    # -Name it?
+    # -Only support for single tracks
+    # -Undo Stack, not REALLY required
+    # -Fairly near done honestly
+    options = Options()
+    try:
+        Original_prompt = "Please enter a command:\n"
+        prompt = Original_prompt
+        while True:
+            commands_in = input(prompt).split(" ")
+            commands_in = [row for row in commands_in if row !=""]
+            try:
+                print(commands_in)
+                prompt = f"{getattr(options, commands_in[0])(commands_in[1:])}\n{Original_prompt}"
+
+            except AttributeError:
+                print('''Execution failed, please choose from the following options:
+remove {range|single} {instrument, name if range, int if single}
+load {song location}
+save {save location}
+play [optional location]
+reset
+exit''')
+    except KeyboardInterrupt:
+        print("Cleaning up /temp directory")
+        shutil.rmtree("temp")
+        print("Exiting now")
+        exit()
+
+class Options:
+    def __init__(self):
+        self.midi_file = MidiFile()
+        self.messages = []
+        self.original_messages = []
+
+    def remove(self, args):
+        scope = args[0]
+        instrument = args[1]
+        try:
+            print(instructions[scope])
+            self.messages = instructions[scope](instrument, self.messages)
+        except KeyError:
+            print(f"{scope} is not a valid option, Valid options are:\n-single\n-range")
+            raise RemoveScopeError
+        return f"{instrument} Removed Successfully"
+
+    def load(self, args):
+        song_location = args[0]
+        if song_location[-4:] != ".mid":
+            song_location += ".mid"
+        try:
+            self.midi_file = MidiFile(f"songs/{song_location}")
+            for message in self.midi_file.tracks[0]:
+                self.messages.append(message)
+            self.original_messages = self.messages
+        except FileNotFoundError:
+            return f"Failed to find songs/{song_location}"
+        return f"{song_location} Loaded Successfully"
+
+    def save(self, args):
+        location = args[0]
+        core.save_messages(self.midi_file, self.messages, location)
+        return "Saved!"
+
+    def play(self, args):
+        if len(args) == 0:
+            core.save_messages(self.midi_file, self.messages)
+            print("Playing, if there's any issues, Ensure you're playing the script as root")
+            core.play_song()
+            os.remove("temp/output.mid")
+        else:
+            try:
+                song_location = args[0]
+                if song_location[-4:] != ".mid":
+                    song_location+=".mid"
+                core.play_song(f"songs/{song_location}")
+            except NotASongLocationException:
+                print(f"{location} is not a valid midi file, please choose another")
+        return "Completed Playing"
+
+    def reset(self, args):
+        self.messages = self.original_messages
+        return "Reset"
+
+    def exit(self, args):
+        raise KeyboardInterrupt
+
+if __name__ == "__main__":
+    if not os.path.exists("./temp"):
+        print("Creating /temp directory")
+        os.mkdir("temp")
+    if not os.path.exists("./songs"):
+        raise NoSongDirectoryException
---
+++ core.py
@@ -0,0 +1,105 @@
+from mido import MidiFile, MidiTrack, Message
+import os
+import sys
+import subprocess
+
+class NoSongDirectoryException(Exception):
+    pass
+
+class NotASongLocationException(Exception):
+    pass
+
+class NoInstrumentError(Exception):
+    pass
+
+class InvalidIntError(Exception):
+    pass
+
+
+def remove_instrument_range(instrument, messages):
+    global instrument_ranges
+    if instrument not in instrument_ranges:
+        print("That is not a Supported instrument,\nPlease try with the following instruments:")
+        for instrument in instrument_ranges.keys():
+            print(f"-{instrument}")
+        raise NoInstrumentError
+    return remove_instrument(instrument, messages, equal_range)
+
+def remove_single_instrument(instrument_num, messages):
+    try:
+        int(instrument_num)
+    except ValueError:
+        print("Please enter a valid Number")
+        raise InvalidIntError
+    if instrument_num not in range(1,128):
+        print("That is not a Supported instrument,\nTry again with a number 0 < n ≤ 127")
+        raise NoInstrumentError
+    return remove_instrument(instrument_num, messages, equal_single)
+
+def equal_single(prog, instrument):
+    return prog == instrument
+
+def equal_range(prog, instrument):
+    global instrument_ranges
+    return prog in instrument_ranges[instrument]
+
+def remove_instrument(instrument, messages, equal_function):
+    ret_messages = []
+    channels_to_be_removed = []
+    for message in messages:
+        if "channel=" in str(message):
+            chan = get_property("channel", str(message))
+            if "program_change" in str(message):
+                prog = get_property("program",str(message))
+                if equal_function(prog, instrument):
+                    channels_to_be_removed.append(chan)
+                ret_messages.append(message)
+            elif chan in channels_to_be_removed and "note_on" in str(message):
+                ret_messages.append(Message("note_on", channel=chan, velocity=0, time=get_property("time",str(message))))
+            else:
+                ret_messages.append(message)
+        else:
+            ret_messages.append(message)
+    return ret_messages
+
+def get_property(prop, message):
+    index = message.index(f"{prop}=")+ (len(prop)+1)
+    try:
+        ret_int = int(message[index:index+3])
+    except ValueError:
+        ret_int = int(message[index:index+2])
+    return ret_int
+
+def save_messages(mido_file, messages, location="temp/output.mid"):
+    mido_file.tracks[0] = messages
+    mido_file.save(location)
+
+def play_song(location="temp/output.mid", quiet=True):
+    if not os.path.exists(location):
+        raise NotASongLocationException
+    else:
+        run_args = ["fluidsynth", "-a", "alsa", "-m", "alsa_seq", "-l", "-i", "Soundfont.sf2", location]
+        if quiet:
+            output = open(os.devnull, "w")
+        else:
+            output = sys.stdout
+        subprocess.call(run_args, stdout=output, stderr=output)
+        
+instrument_ranges = {"piano": range(1,9),
+                     "Chromatic Percussion":range(9,17),
+                     "Organ": range(17,25),
+                     "guitar":range(25,33),
+                     "Bass": range(30,41),
+                     "Orchestra Solo": range(41,49),
+                     "Orchestra Ensemble": range(49,57),
+                     "Brass": range(57,65),
+                     "Reed": range(65,73),
+                     "Wind": range(73,81),
+                     "Synth Lead": range(81,89),
+                     "Synth Pad": range(89,97),
+                     "Synth Sound FX": range(97,105),
+                     "Ethnic": range(105,113),
+                     "Percusssive": range(113,121),
+                     "Misc SFX": range(121,129),
+                     }
+
